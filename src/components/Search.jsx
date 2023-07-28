5ec77884f384b315f6548bed0fa92f76f2515192
exb <radmir_96@mail.ru>
2023-07-28 16:56:09 +0500

    success fetch data

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,38 @@
-import {useState} from 'react'
+import {debounce} from 'lodash'
+import {useCallback, useState} from 'react'
 import {Text, TextInput, TouchableOpacity, View} from 'react-native'
 import {MagnifyingGlassIcon} from 'react-native-heroicons/outline'
 import {MapPinIcon} from 'react-native-heroicons/solid'
 
+import {fetchLocation, fetchWeatherForecast} from '../config/api'
+import Forecast from './Forecast'
+
 const Search = () => {
   const [toggleSearch, setToggleSearch] = useState(false)
-  const [location, setLocation] = useState([1, 2, 3])
+  const [location, setLocation] = useState([])
+  const [weather, setWeather] = useState({})
 
-  const handleLocation = (loc) => {
-    console.log('location')
+  const handleLocation = (item) => {
+    setLocation([])
+    fetchWeatherForecast({cityName: item.name, days: '7'}).then((data) => {
+      setWeather(data)
+    })
   }
+
+  const handleSearch = (value) => {
+    if (value.length > 2) {
+      fetchLocation({cityName: value})
+        .then((data) => {
+          setLocation(data)
+        })
+        .catch((error) => {
+          console.log('Error in handleSearch:', error)
+        })
+    }
+  }
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 1200), [])
+  const {loc, current} = weather
 
   return (
     <View className="mx-4 relative z-50">
@@ -22,6 +45,7 @@ const Search = () => {
         }}>
         {toggleSearch ? (
           <TextInput
+            onChangeText={handleTextDebounce}
             className="flex-1 pl-6 text-base text-white"
             placeholder="Search city"
             placeholderTextColor={'lightgray'}
@@ -35,18 +59,17 @@ const Search = () => {
       </View>
       {location.length > 0 && toggleSearch ? (
         <View className="absolute bg-gray-300 w-full rounded-3xl top-16">
-          // TODO: add data from api
           {location.map((item, index) => {
             let showBorder = index + 1 != location.length
             let borderClass = showBorder ? ' border-b-2 border-b-gray-400' : ''
             return (
               <TouchableOpacity
-                onPress={() => handleLocation(loc)}
+                onPress={() => handleLocation(item)}
                 key={index}
                 className={'flex-row items-center p-3 px-4 mb-1' + borderClass}>
                 <MapPinIcon size={20} color={'gray'} />
                 <Text className="text-black text-lg ml-2">
-                  Ufa, Bashkortostan
+                  {item?.name}, {item?.country}
                 </Text>
               </TouchableOpacity>
             )
---
+++ src/config/api.js
@@ -0,0 +1,35 @@
+const apiKey = '2a4333ed5d5f4e93b50101803232807'
+
+const forecastEndpoint = (params) =>
+  `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${params.cityName}&days=${params.days}&aqi=no&alerts=no`
+const locationEndpoint = (params) =>
+  `https://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${params.cityName}`
+
+const fetchData = async (endpoint) => {
+  try {
+    const response = await fetch(endpoint)
+    if (!response.ok) {
+      throw new Error('Error occured!')
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.log('Error from fetchData', error)
+    throw error
+  }
+}
+
+export const fetchWeatherForecast = (params) => {
+  return fetchData(forecastEndpoint(params))
+}
+
+export const fetchLocation = async (params) => {
+  try {
+    const data = await fetchData(locationEndpoint(params))
+    console.log('Location data:', data)
+    return data
+  } catch (error) {
+    console.log('Error from fetchLocation', error)
+    throw error
+  }
+}
